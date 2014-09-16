24dee1a96d3e6017479dbb452018cd9ee91c7693
konstantinnovation <konstans@stuycs.org>
2014-09-16 13:00:33 -0400

    middle of class

--- greeter/p7/demo.java
+++ greeter/p7/demo.java
@@ -4,6 +4,13 @@ public class demo{
 	bob = new greeter();
 	amy = new greeter("Hola");
 	bob.greet();
+	bob.greet();
+	bob.greet();
+	bob.greet();
 	amy.greet();
+	bob.printCounter();
+	//System.out.println(bob.counter);
+	//System.out.println(amy.greeting);
+	
     }
 }
--- greeter/p7/greeter.java
+++ greeter/p7/greeter.java
@@ -1,20 +1,27 @@
 public class greeter{
     //instance variables - the information each object can remember
-    String greeting;
+    private String greeting;
+    private int counter;
 
     //constructor - the method that is called to instantiate an object
     public greeter(){
 	greeting = "Hello!!!";
+	counter = 0;
     }
 
     public greeter(String s){
 	greeting = s;
+	counter = 0;
     }
+    //
+    public void printCounter(){
+	System.out.println(counter);
+    }
+
 
     //other methods
     public void greet(){
 	System.out.println(greeting);
+	counter = counter + 1;
     }
-
-
 }
